ac6c891f918e2608c8a5471d66d614f1f1abfbaf
Asphalia <j.adam@asphalia.fr>
2024-06-09 10:27:07 +0000

    cleanup

--- custom_components/bdx_pmv/sensor.py
+++ custom_components/bdx_pmv/sensor.py
@@ -1,7 +1,5 @@
-from datetime import timedelta, date
+from datetime import timedelta
 import logging
-import re
-import json
 
 import voluptuous as vol
 
@@ -11,8 +9,7 @@ from homeassistant.helpers.entity import Entity
 import requests
 
 from .const import (
-    DOMAIN, CONF_KEY, CONF_GID, CONF_SCAN_INTERVAL,
-    ATTR_PAGE1, ATTR_PAGE2, ATTR_COUNT, CONF_DAYS_OLD)
+    CONF_KEY, CONF_IDENT, ATTR_PAGE)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -21,70 +18,57 @@ SCAN_INTERVAL = timedelta(seconds=30*60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_KEY): cv.string,
-    vol.Required(CONF_GID, default='5742'): cv.string,
-    # vol.Required(CONF_SCAN_INTERVAL, default=3600): cv.positive_int,
-    vol.Required(CONF_DAYS_OLD, default='30'): cv.positive_int,
+    vol.Required(CONF_IDENT, default='Z40P115'): cv.string,
 })
 
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the PMV platform."""
-    add_entities([PMVEntity(config)], True)
+    pages = ['page1', 'page2']
+    entities = [PMVEntity(config, page) for page in pages]
+    add_entities(entities, True)
 
 
 class PMVEntity(Entity):
     """PMV Entity."""
 
-    def __init__(self, config):
+    def __init__(self, config, page):
         """Init the PMV Entity."""
         self._attr = {
-            ATTR_PAGE1: {},
-            ATTR_PAGE2: {},
-	        ATTR_COUNT: 0
+            ATTR_PAGE: {}
         }
 
         self.bdx_key = config[CONF_KEY]
-        self.gid = config[CONF_GID]
+        self.ident = config[CONF_IDENT]
+        self.page = page
 
     def update(self):
         """Update data."""
         self._attr = {
-            ATTR_PAGE1: {},
-            ATTR_PAGE2: {},
-	        ATTR_COUNT: 0
+            ATTR_PAGE: {}
         }
 
-        # update to current day
-        # self.flag = [u'SINCE', date.today() - timedelta(days=self.days_old)]
-        # _LOGGER.debug(f'flag: {self.flag}')
-
-        message1 = ""
-        message2 = ""
         url = "https://data.bordeaux-metropole.fr/geojson?key={}&typename=pc_pmv_p".format(self.bdx_key)
         response = requests.get(url)
         data = response.json()
         _LOGGER.debug(f'json data: {data}')
         for feature in data['features']:
-            if feature['properties']['gid'] == int(self.gid):
-                message1 = feature['properties']['page1']
-                message2 = feature['properties']['page2']
+            if feature['properties']['ident'] == self.ident:
+                self._attr[ATTR_PAGE] = feature['properties'][self.page]
                 break
 
-        self._attr[ATTR_PAGE1] = message1
-        self._attr[ATTR_PAGE2] = message2
-        _LOGGER.debug(f'ATTR_PAGE1: {self._attr[ATTR_PAGE1]}')
-        _LOGGER.debug(f'ATTR_PAGE2: {self._attr[ATTR_PAGE2]}')
+        _LOGGER.debug(f'ATTR_PAGE: {self._attr[ATTR_PAGE]}')
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'pmv_{}'.format(self.gid)
+        return 'pmv_{}_{}'.format(self.ident, self.page)
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._attr[ATTR_PAGE1]
+        return self._attr[ATTR_PAGE]
 
     @property
     def extra_state_attributes(self):
